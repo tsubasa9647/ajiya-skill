51cb21f16481ba4c58d85765e68d70db7f128a90
Tsubasa Imamura <imamura@mk.ces.kyutech.ac.jp>
2019-07-08 19:49:24 +0900

    nineth commit

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,23 @@
+require 'http'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+
 class MenusController < ApplicationController
   def index
-    render json: {menu: "maeda"}
+    ajiya_uri = 'http://ajiya1.com/'
+    ajiya_page = URI::parse(ajiya_uri).read()
+    ajiya_doc = Nokogiri::HTML::parse(ajiya_page)
+
+    dairy_uri = ajiya_doc.css('.rpwe-block').css('.rpwe-title').css('a')[0][:href]
+    # puts dairy_uri
+
+    dairy_page = URI::parse(dairy_uri).read()
+    dairy_doc = Nokogiri::HTML::parse(dairy_page)
+
+    bento = dairy_doc.css('.entry-content')
+    bento_str = bento.to_html.gsub(%r{</?[^>]+?>},'').strip.gsub('●','').chomp
+
+    puts bento_str
   end
 end
